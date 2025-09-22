d66bfc40c6d4b3c580044cf9d56d79a9cf866e1e
Craig <crag.efc@gmail.com>
2025-09-22 17:33:19 +0100

    removed debug and cleaned up login spec tests

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -2,23 +2,20 @@ import 'dotenv/config';
 import { test, expect } from '@playwright/test';
 import { TechTest } from '../main/base/techTest';
 
-let tt: TechTest 
+let tt: TechTest
 
-// test.beforeEach(async ({ page }) => {
-//     
-//     await tt.goto('/', page);
-// });
+test.beforeEach(async ({ page }) => {
+    tt = new TechTest('http://3.8.242.61', page);
+    await tt.goto('/');
+});
+
 
 test('user can login', async ({ page }) => {
-   const tt = new TechTest('http://3.8.242.61', page);
-await tt.goto('/');
-await tt.login(process.env.EMAIL!, process.env.PASSWORD!);
+    await tt.login(process.env.EMAIL!, process.env.PASSWORD!);
 });
 
 test('user enters invalid login details', async ({ page }) => {
-    await tt.goto('/', page);
-
-    await tt.login(page, 'invalid@duck.com', 'wrongpassword');
-    const errorMessage = await tt.getByText(page, 'Invalid login attempt.');
+    await tt.login('invalid@duck.com', 'wrongpassword');
+    const errorMessage = await tt.getByText('Invalid login attempt.');
     await expect(errorMessage).toBeVisible();
-    });
+});
